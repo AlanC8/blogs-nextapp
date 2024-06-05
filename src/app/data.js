44f811dd3f4b6114daa7bca2804160c5fd3fe187
AlanC8 <askvizi@bk.ru>
2024-06-05 16:32:51 +0500

    1.0.0: First commit

---
+++ src/app/data.js
@@ -0,0 +1,54 @@
+const data = [
+  {
+    id: 1,
+    date: "2024-06-04",
+    author: "Baurma",
+    title: "How to Sneak Your Phone into Class Like a Pro",
+    image_url:
+      "https://get.wallhere.com/photo/woods-gorge-bay-rocks-666808.jpg",
+    description:
+      "Tired of getting caught? Here are 10 ninja-level tips to keep your phone hidden and your texts flowing during boring lectures.",
+  },
+  {
+    id: 2,
+    date: "2024-06-03",
+    author: "Daulet Issatayev",
+    title: "10 Reasons Why Avocado Toast is the Secret to Happiness",
+    image_url:
+      "https://i.pinimg.com/originals/43/3a/81/433a815b8313453ee2edae5f7c91de73.jpg",
+    description:
+      "You won't believe number 7! Find out why this trendy snack is more than just a food – it’s a lifestyle.",
+  },
+  {
+    id: 3,
+    date: "2024-06-02",
+    author: "Madina ",
+    title: "Memes That Will Make Your Mom Say 'What?'",
+    image_url:
+      "https://i.pinimg.com/originals/e1/98/cd/e198cd2c0c51be488c3f9f80e19b5de0.png",
+    description:
+      "From Spongebob to Baby Yoda, these memes will have you laughing while leaving your parents totally confused.",
+  },
+  {
+    id: 4,
+    date: "2024-06-01",
+    author: "Alibek Seitov",
+    title: "Why Homework is Basically DLC for School",
+    image_url:
+      "https://i.pinimg.com/originals/0b/76/01/0b760185d3a88d9648775751beff3ef5.png",
+    description:
+      "Ever feel like homework is just extra content you didn’t ask for? Here’s why it’s the ultimate downloadable content for your education.",
+  },
+  {
+    id: 5,
+    date: "2024-05-31",
+    author: "Uldana",
+    title: "The Ultimate Guide to Going Viral on TikTok",
+    image_url:
+      "https://i.pinimg.com/originals/43/3a/81/433a815b8313453ee2edae5f7c91de73.jpg",
+    description:
+      "Want to be the next big thing? Follow these simple steps to become a TikTok sensation overnight (or at least get a few likes).",
+  },
+];
+
+export default data;
